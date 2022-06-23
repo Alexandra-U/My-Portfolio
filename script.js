c129fe2d82f86de1a342b21947e8445b4343242d
Alexandra <alexandraurian1@gmail.com>
2022-06-23 14:28:07 +0300

    Update

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".btn1").onclick = function() {
+    window.location.href='3d.html';
+}
+
+document.querySelector(".btn2").onclick = function() {
+    window.location.href='drawing.html';
+}
+
+document.querySelector(".btn3").onclick = function() {
+    window.location.href='ill.html';
+}
