652e6fa88994139f60a8d5a548cfb4903aba0c4b
Alexandra <alexandraurian1@gmail.com>
2022-06-27 22:29:12 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.querySelector(".btn1").onclick = function() {
     window.location.href='3d.html';
 }
@@ -9,3 +10,38 @@ document.querySelector(".btn2").onclick = function() {
 document.querySelector(".btn3").onclick = function() {
     window.location.href='ill.html';
 }
+
+// window.onbeforeunload = function() {
+//     document.querySelector(".cover").onclick = function(e) {
+//         document.querySelector(".cover").style.display = "none";
+    
+  
+// }
+//    }
+
+ let hideCover = document.querySelector(".cover");
+ window.addEventListener("load", function(){
+    hideCover.onclick = function(event) {
+        hideCover.remove();
+
+
+        
+        event.preventDefault();
+    }
+ });
+
+
+  
+
+  
+
+
+
+   
+
+    
+
+
+
+
+
