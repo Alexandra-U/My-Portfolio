bcf51fae2c1566c525f42f9c06d3ebaca3d0bd8e
Alexandra <alexandraurian1@gmail.com>
2022-06-30 18:59:59 +0300

    Update

--- script.js
+++ script.js
@@ -11,46 +11,14 @@ document.querySelector(".btn3").onclick = function() {
     window.location.href='ill.html';
 }
 
-// modal
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.querySelector(".test");
-var modalImg = document.getElementById("img01");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-// modal end
-
-
-// window.onbeforeunload = function() {
-//     document.querySelector(".cover").onclick = function(e) {
-//         document.querySelector(".cover").style.display = "none";
-    
-  
-// }
-//    }
 
  let hideCover = document.querySelector(".cover");
- window.addEventListener("click", () => {
-        hideCover.remove();    
+ window.addEventListener("click", (e) => {
+        hideCover.remove(); 
         
-        document.querySelector('.cover').addEventListener('click', () => {
-            history.back();
-          });
-
-    
+        
+        e.preventDefault();
     
  });
 
